b8f86bf79853d6d8a726618fa532c44daaa4ca6f
muhammad wajih <wajih898@gmail.com>
2026-06-28 12:55:31 +0500

    SQL installed

--- MyProject/Myapp/urls.py
+++ MyProject/Myapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import landing_page
+from .views import *
 
 urlpatterns = [
     path('', landing_page, name='home'),
+    path('about/', about_page, name='about'),
 ]
 
+  
--- MyProject/Myapp/views.py
+++ MyProject/Myapp/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def landing_page(request):
     return render(request, 'index.html')
+
+
+def about_page(request):
+    return render(request, 'about.html')
